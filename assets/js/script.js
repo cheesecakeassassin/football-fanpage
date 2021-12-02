274b3e88c2ed7e85beca42a3431a829702b08a21
cheesecake_assassin <santasebastian@yahoo.com>
2021-12-01 21:08:26 -0500

    testing 2 apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var requestUrl = 'https://www.scorebat.com/video-api/v3/';
+var highlightsApi = 'https://www.scorebat.com/video-api/v3/';
+var standingsApi = 'https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2020&sort=asc&per_page=5';
 
-fetch(requestUrl)
+fetch(highlightsApi)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('Standings \n----------');
+    console.log('Highlights \n----------');
     console.log(data);
 
     // TODO: Loop through the response
@@ -13,5 +14,20 @@ fetch(requestUrl)
     {
     // TODO: Console log each issue's URL and each user's login
     console.log(data.response[i].title);
+    }
+  });
+
+  fetch(standingsApi)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Standings \n----------');
+    console.log(data);
+    console.log(data.data.standings);
+
+    for (var i = 0; i < data.data.standings.length; i++)
+    {
+    console.log(data.data.standings[i].team.name);
     }
   });
