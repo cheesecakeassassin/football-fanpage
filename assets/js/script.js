8824e9f7715725e936f52c0974746580194e24da
cheesecake_assassin <santasebastian@yahoo.com>
2021-12-01 20:57:52 -0500

    connecting api demo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var requestUrl = 'https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2021&sort=asc';
+var requestUrl = 'https://www.scorebat.com/video-api/v3/';
 
 fetch(requestUrl)
   .then(function (response) {
@@ -7,11 +7,11 @@ fetch(requestUrl)
   .then(function (data) {
     console.log('Standings \n----------');
     console.log(data);
+
     // TODO: Loop through the response
-    // for (var i = 0; i < data.length; i++)
-    // {
-    // // TODO: Console log each issue's URL and each user's login
-    // console.log(data[i]);
-    // console.log(data[i].user.login);
-    // }
+    for (var i = 0; i < data.response.length; i++)
+    {
+    // TODO: Console log each issue's URL and each user's login
+    console.log(data.response[i].title);
+    }
   });
