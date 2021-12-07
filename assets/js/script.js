6498cfe59a9aa22e75a077f299b1b9c513151376
mohamed0228 <mohamed0228@outlook.fr>
2021-12-06 19:37:09 -0500

    enhacements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
+var selectOne = document.querySelector('.one')
+var selectLeague = document.querySelector(".company")
+
 var highlightsApi = 'https://www.scorebat.com/video-api/v3/';
-var standingsApi = 'https://api-football-standings.azharimm.site/leagues';
+var standingsApi = ' https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2021&sort=asc';
+var leagueApi =' https://api-football-standings.azharimm.site/leagues';
+
+
 
 fetch(highlightsApi)
   .then(function (response) {
@@ -7,13 +13,19 @@ fetch(highlightsApi)
   })
   .then(function (data) {
     console.log('Highlights \n----------');
-    console.log(data);
+   console.log(data);
 
     // TODO: Loop through the response
     for (var i = 0; i < data.response.length; i++)
     {
+      //console.log(data.response[i]);
+      
+      
+      //var listItem =document.createElement('li')
+      //listItem.textContent = data.response[i];
+      //selectOne.appendChild(listItem);
     // TODO: Console log each issue's URL and each user's login
-    console.log(data.response[i].title);
+    //console.log(data.response[i].title);
     }
   });
 
@@ -23,11 +35,42 @@ fetch(highlightsApi)
   })
   .then(function (data) {
     console.log('Standings \n----------');
-    console.log(data);
-    console.log(data.data.standings);
+    //console.log(data);
+    //console.log(data.data.standings);
 
     for (var i = 0; i < data.data.standings.length; i++)
     {
-    console.log(data.data.standings[i].team.name);
+      
+      var listItem =document.createElement('li')
+      listItem.textContent = data.data.standings[i].team.name;
+      selectOne.appendChild(listItem);
+    console.log(data.data.standings[i].team);
+    //console.log(data.data.standings[i].team.name);
     }
   });
+
+// selectLeague downdrop
+fetch(leagueApi)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Highlights \n----------');
+   //console.log(data.data);
+
+    // TODO: Loop through the response
+    for (var i = 0; i < data.data.length; i++)
+    {
+
+      console.log(data.data[i].slug);
+      
+      var leagues =data.data[i].name;
+      var listOption =document.createElement('option')
+      listOption.textContent = leagues;
+       selectLeague.appendChild(listOption);
+       
+    
+    }
+  });
+
+  
