0723ad99d7229fee0e23700032c1aa5ed574e33c
cheesecake_assassin <santasebastian@yahoo.com>
2021-12-09 23:25:17 -0500

    hardcoding leagues to show in dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,15 @@ var selectLeague = document.querySelector("#three");
 var selectVid = document.querySelector("#vid");
 
 var leagueId = "eng.1"; // The default league shown is the English Premier League
+var year = "2021"; // The default year will be 2021
+var leagues;
+var listOption;
 
 var highlightsApi = "https://www.scorebat.com/video-api/v3/";
 var standingsApi =
   " https://api-football-standings.azharimm.site/leagues/" +
   leagueId +
-  "/standings?season=2021&sort=asc";
+  "/standings?season=" + year + "&sort=asc";
 var leagueApi = " https://api-football-standings.azharimm.site/leagues";
 
 fetch(highlightsApi)
@@ -59,43 +62,16 @@ fetch(leagueApi)
     return response.json();
   })
   .then(function (data) {
+    console.log("Leagueeeeeee");
     console.log(data.data);
 
     // TODO: Loop through the response
     for (var i = 0; i < data.data.length; i++) {
-      var leagues = data.data[i].name;
-      var listOption = document.createElement("option");
-      listOption.textContent = leagues;
-      selectLeague.appendChild(listOption);
+      if (i == 5 || i == 6 || i == 7 || i == 9 || i == 13 || i == 16) {
+        leagues = data.data[i].name;
+        listOption = document.createElement("option");
+        listOption.textContent = leagues;
+        selectLeague.appendChild(listOption);
+      }
     }
   });
-
-var count = 0;
-
-// TODO: Add a comment describing the functionality of the following document.querySelector() methods:
-// Initializing variables for the event listener (increment, decrement, count)
-var incrementEl = document.querySelector("#increment");
-var decrementEl = document.querySelector("#decrement");
-var countEl = document.querySelector("#count");
-
-// TODO: Add a comment describing the functionality of the following code:
-// Sets countEl element with count variable which was previously initialized as 0
-function setCounterText() {
-  countEl.textContent = leagueId;
-}
-// TODO: Add a comment describing the functionality of the following event listener:
-// Event listener for incrementing the count when increment is clicked
-incrementEl.addEventListener("click", function () {
-  count++;
-  setCounterText();
-});
-
-// TODO: Add a comment describing the functionality of the following event listener:
-// Event listener for decrementing the count when decrement is clicked
-decrementEl.addEventListener("click", function () {
-  // Action will fire if count is greater than  0
-  if (count > 0) {
-    count--;
-    setCounterText();
-  }
-});
