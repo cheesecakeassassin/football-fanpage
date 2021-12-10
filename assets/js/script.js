918096d00687401108419cef4b2d5a2c8735b2ab
cheesecake_assassin <santasebastian@yahoo.com>
2021-12-09 19:09:18 -0500

    formatting code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,54 @@
-var selectOne = document.querySelector('.one')
-var selectTwo = document.querySelector('#two')
-var selectLeague = document.querySelector("#three")
-var selectVid = document.querySelector("#vid")
-var highlightsApi = 'https://www.scorebat.com/video-api/v3/';
-var standingsApi = ' https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2021&sort=asc';
-var leagueApi =' https://api-football-standings.azharimm.site/leagues';
+var selectOne = document.querySelector(".one");
+var selectTwo = document.querySelector("#two");
+var selectLeague = document.querySelector("#three");
+var selectVid = document.querySelector("#vid");
 
-
+var highlightsApi = "https://www.scorebat.com/video-api/v3/";
+var standingsApi =
+  " https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2021&sort=asc";
+var leagueApi = " https://api-football-standings.azharimm.site/leagues";
 
 fetch(highlightsApi)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('Highlights \n----------');
-   
+    console.log("Highlights \n----------");
 
     // TODO: Loop through the response
-    for (var i = 0; i < 3; i++)
-    {
+    for (var i = 0; i < 3; i++) {
       var competitions = data.response[i].competition;
-      var listOption2 = document.createElement('option');
+      var listOption2 = document.createElement("option");
       listOption2.textContent = competitions;
       selectTwo.appendChild(listOption2);
-      //////
+
       var video = data.response[i].videos[0].embed;
-      var listVideo = document.createElement('div');
-      //console.log(video)
-       /*listVideo.innerHTML=`<video width="320" height="240" controls>
-       <source src="${video}" type="video/mp4">
-       <source src="${video}" type="video/ogg">
-       Your browser does not support the video tag.
-     </video>`;*/
-     listVideo.innerHTML=video;
-       selectVid.appendChild(listVideo);
-       
+      var listVideo = document.createElement("div");
+
+      listVideo.innerHTML = video;
+      selectVid.appendChild(listVideo);
 
       console.log(data.response[i].videos[0]);
-      
     }
   });
 
-  fetch(standingsApi)
+fetch(standingsApi)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('Standings \n----------');
+    console.log("Standings \n----------");
     //console.log(data);
     //console.log(data.data.standings);
 
-    for (var i = 0; i < data.data.standings.length; i++)
-    {
+    for (var i = 0; i < data.data.standings.length; i++) {
       //console.log (data.data.standings[i])
-      var standings =data.data.standings[i].team.name;
-      var listItem =document.createElement('li');
+      var standings = data.data.standings[i].team.name;
+      var listItem = document.createElement("li");
       listItem.textContent = standings;
       selectOne.appendChild(listItem);
-    //console.log(data.data.standings[i].team);
-    //console.log(data.data.standings[i].team.name);
+      //console.log(data.data.standings[i].team);
+      //console.log(data.data.standings[i].team.name);
     }
   });
 
@@ -68,22 +58,16 @@ fetch(leagueApi)
     return response.json();
   })
   .then(function (data) {
-    console.log('Highlights \n----------');
-   //console.log(data.data);
+    console.log("Highlights \n----------");
+    //console.log(data.data);
 
     // TODO: Loop through the response
-    for (var i = 0; i < data.data.length; i++)
-    {
-
+    for (var i = 0; i < data.data.length; i++) {
       //console.log(data.data[i].slug);
-      
-      var leagues =data.data[i].name;
-      var listOption =document.createElement('option')
+
+      var leagues = data.data[i].name;
+      var listOption = document.createElement("option");
       listOption.textContent = leagues;
-       selectLeague.appendChild(listOption);
-       
-    
+      selectLeague.appendChild(listOption);
     }
   });
-
-  
