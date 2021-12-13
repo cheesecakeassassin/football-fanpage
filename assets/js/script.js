702ad5def2e33204cd07514909fa5803b8f38a69
cheesecake_assassin <santasebastian@yahoo.com>
2021-12-13 01:22:46 -0500

    connecting modal buttons to league standings

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,62 +11,6 @@ var standingsApi =
   "/standings?season=2021&sort=asc";
 var leagueApi = " https://api-football-standings.azharimm.site/leagues";
 
-// Fetches the API that shows football league standings from different competitions
-fetch(standingsApi)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    // Creates variable to make the API values easier for humans to understand
-    var standingsInfo = data.data.standings;
-
-    // Iterates through every team in the selected league in order to create table of stats
-    for (var i = 0; i < standingsInfo.length; i++) {
-      // Creates a row in the table for a new team
-      var teamEl = document.createElement("tr");
-      teamEl.className = standingsInfo[i];
-
-      // Creates an element on the row for each of the team's stats
-      var position = document.createElement("th");
-      var teamName = document.createElement("td");
-      var played = document.createElement("td");
-      var wins = document.createElement("td");
-      var draws = document.createElement("td");
-      var losses = document.createElement("td");
-      var goalsFor = document.createElement("td");
-      var goalsAgainst = document.createElement("td");
-      var goalDifference = document.createElement("td");
-      var points = document.createElement("td");
-
-      // Gets the team's stats from the API and inserting them where they go
-      position.textContent = i + 1; // Gets index and adds 1 to get team's league position
-      teamName.textContent = standingsInfo[i].team.name;
-      played.textContent = standingsInfo[i].stats[3].value;
-      wins.textContent = standingsInfo[i].stats[0].value;
-      draws.textContent = standingsInfo[i].stats[2].value;
-      losses.textContent = standingsInfo[i].stats[1].value;
-      goalsFor.textContent = standingsInfo[i].stats[4].value;
-      goalsAgainst.textContent = standingsInfo[i].stats[5].value;
-      goalDifference.textContent = standingsInfo[i].stats[9].value;
-      points.textContent = standingsInfo[i].stats[6].value;
-
-      // Appends team's stats to the new row
-      teamEl.appendChild(position);
-      teamEl.appendChild(teamName);
-      teamEl.appendChild(played);
-      teamEl.appendChild(wins);
-      teamEl.appendChild(draws);
-      teamEl.appendChild(losses);
-      teamEl.appendChild(goalsFor);
-      teamEl.appendChild(goalsAgainst);
-      teamEl.appendChild(goalDifference);
-      teamEl.appendChild(points);
-
-      // Adding completed team row to the table
-      standingsTable.appendChild(teamEl);
-    }
-  });
-
 // leaguesDropdown downdrop
 fetch(leagueApi)
   .then(function (response) {
@@ -101,13 +45,13 @@ mySel.addEventListener("change", function () {
 
 function leagueSelection() {
   standingsTable.innerHTML = "";
-  
-  standingsApi =
-  " https://api-football-standings.azharimm.site/leagues/" +
-  leagueId +
-  "/standings?season=2021&sort=asc";
-  console.log(standingsApi);
 
+  standingsApi =
+    " https://api-football-standings.azharimm.site/leagues/" +
+    leagueId +
+    "/standings?season=2021&sort=asc";
+
+  // Fetches the API that shows football league standings from different competitions
   fetch(standingsApi)
     .then(function (response) {
       return response.json();
@@ -120,7 +64,6 @@ function leagueSelection() {
       for (var i = 0; i < standingsInfo.length; i++) {
         // Creates a row in the table for a new team
         var teamEl = document.createElement("tr");
-        teamEl.className = standingsInfo[i];
 
         // Creates an element on the row for each of the team's stats
         var position = document.createElement("th");
@@ -178,78 +121,65 @@ var storage = document.querySelector(".store");
 var retrievedObject = localStorage.getItem("leagueName");
 document.querySelector(".box").onclick = function (event) {
   var leagueName = event.target.innerHTML;
-  localStorage.setItem("leagueName", JSON.stringify(leagueName));
+  leagueId = event.target.id;
+  localStorage.setItem("leagueId", leagueId);
+  localStorage.setItem("leagueName", leagueName);
 
-  console.log("retrievedObject: ", JSON.parse(retrievedObject));
   var listPl = document.createElement("button");
   listPl.className = "button is-black is-rounded is-outlined btns";
   listPl.textContent = leagueName;
+  console.log(listPl);
   storage.appendChild(listPl);
+
+  leagueSelection();
 };
 
 function myFunction() {
-  var listPl =document.createElement('button');
-  listPl.className="button is-black is-rounded is-outlined btns";
-  listPl.textContent= retrievedObject;
-  storage.appendChild(listPl);
+  var listPl = document.createElement("button");
+  listPl.className = "button is-black is-rounded is-outlined btns";
+
+  if (retrievedObject != null) {
+    listPl.textContent = retrievedObject;
+    storage.appendChild(listPl);
+  }
 }
 
 myFunction();
 
 // Script for modal in the footer
-const modal = 
-            document.querySelector('.modal');
-    const btn = 
-            document.querySelector('#btn')
-      const close1 = 
-            document.querySelector('.btn1')
-      const close2 = 
-            document.querySelector('.btn2')
-      const close3 = 
-            document.querySelector('.btn3')
-      const close4 = 
-            document.querySelector('.btn4')
-      const close5 = 
-            document.querySelector('.btn5')
-      const close6 = 
-            document.querySelector('.btn6')
-      const close7 = 
-            document.querySelector('.btn2')  
-    
-      btn.addEventListener('click',
-                           function () {
-        modal.style.display = 'block'
-        
-      })
-      
-      close1.addEventListener('click',
-                             function () {
-        modal.style.display = 'none'
-      })
-      close2.addEventListener('click',
-                             function () {
-        modal.style.display = 'none'
-      })
-      close3.addEventListener('click',
-                             function () {
-        modal.style.display = 'none'
-      })
-      close4.addEventListener('click',
-                             function () {
-        modal.style.display = 'none'
-      })
-      close5.addEventListener('click',
-                             function () {
-        modal.style.display = 'none'
-      })
-      close6.addEventListener('click',
-                             function () {
-        modal.style.display = 'none'
-      })
-      window.addEventListener('click',
-                              function (event) {
-        if (event.target.className === 
-            'modal-background') {
-          modal.style.display = 'none'
-        }
-      })
+const modal = document.querySelector(".modal");
+const btn = document.querySelector("#btn");
+const close1 = document.querySelector(".btn1");
+const close2 = document.querySelector(".btn2");
+const close3 = document.querySelector(".btn3");
+const close4 = document.querySelector(".btn4");
+const close5 = document.querySelector(".btn5");
+const close6 = document.querySelector(".btn6");
+
+btn.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
+close1.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+close2.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+close3.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+close4.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+close5.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+close6.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+window.addEventListener("click", function (event) {
+  if (event.target.className === "modal-background") {
+    modal.style.display = "none";
+  }
+});
