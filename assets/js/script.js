ac034076873f84596d59c2a81d9a3c1f274a3673
Sebastian Santa <santasebastian@yahoo.com>
2023-02-28 16:40:15 -0500

    Removing unnecessary year from dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ var retrievedObject = localStorage.getItem("leagueName");
 var year = localStorage.getItem("year");
 
 // Constant to declare an array of the years users can select in the dropdown
-const yearsArray = ["2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006"];
+const yearsArray = ["2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006"];
 
 // Variable that contains the favorite league button found on the footer of the page
 var favoriteLeagueButton = document.querySelector(".favorite-league-button");
